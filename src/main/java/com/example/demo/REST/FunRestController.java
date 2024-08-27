7e343c62b893eda213768847fe20444deab2b2c2
abjeet16 <avjeetyadav00@gmail.com>
2024-08-27 18:41:09 +0530

    learning

--- src/main/java/com/example/demo/REST/FunRestController.java
+++ src/main/java/com/example/demo/REST/FunRestController.java
@@ -1,10 +1,21 @@
 package com.example.demo.REST;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+    @GetMapping("/teamInfo")
+    public String getTeamInfo(){
+        return "Coach name : "+coachName+", Team name : "+teamName;
+    }
     @GetMapping("/")
     public String sayHello(){
         return "Hello Abjeet";
